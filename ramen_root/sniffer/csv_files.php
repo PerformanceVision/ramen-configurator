4a32f5728f55380fa8447ecd8b58bc7c3a9442d1
Cedric Cellier <rixed@happyleptic.org>
2019-09-18 06:05:32 +0200

    Makes all CSV parsers lazy
    Won't change much, as the metrics are persistent.
    
    Ref #44

--- ramen_root/sniffer/csv_files.php
+++ ramen_root/sniffer/csv_files.php
@@ -6,91 +6,91 @@ PARAMETERS
   csv_compressed DEFAULTS TO false,
   csv_delete DEFAULTS TO true;
 
-DEFINE tcp AS
+DEFINE LAZY tcp AS
   READ FROM
     FILES csv_prefix || "tcp_v29.*csv" || (IF csv_compressed THEN ".lz4" ELSE "")
       PREPROCESSED WITH (IF csv_compressed THEN "lz4 -d -c" ELSE "")
       THEN DELETE IF csv_delete
     <?=$tcp?>;
 
-DEFINE udp AS
+DEFINE LAZY udp AS
   READ FROM
     FILES csv_prefix || "udp_v29.*csv" || (IF csv_compressed THEN ".lz4" ELSE "")
       PREPROCESSED WITH (IF csv_compressed THEN "lz4 -d -c" ELSE "")
       THEN DELETE IF csv_delete
     <?=$udp?>;
 
-DEFINE icmp AS
+DEFINE LAZY icmp AS
   READ FROM
     FILES csv_prefix || "icmp_v29.*csv" || (IF csv_compressed THEN ".lz4" ELSE "")
       PREPROCESSED WITH (IF csv_compressed THEN "lz4 -d -c" ELSE "")
       THEN DELETE IF csv_delete
     <?=$icmp?>;
 
-DEFINE 'other-ip' AS
+DEFINE LAZY 'other-ip' AS
   READ FROM
     FILES csv_prefix || "other_ip_v29.*csv" || (IF csv_compressed THEN ".lz4" ELSE "")
       PREPROCESSED WITH (IF csv_compressed THEN "lz4 -d -c" ELSE "")
       THEN DELETE IF csv_delete
     <?=$otherip?>;
 
-DEFINE 'non-ip' AS
+DEFINE LAZY 'non-ip' AS
   READ FROM
     FILES csv_prefix || "non_ip_v29.*csv" || (IF csv_compressed THEN ".lz4" ELSE "")
       PREPROCESSED WITH (IF csv_compressed THEN "lz4 -d -c" ELSE "")
       THEN DELETE IF csv_delete
     <?=$nonip?>;
 
-DEFINE dns AS
+DEFINE LAZY dns AS
   READ FROM
     FILES csv_prefix || "dns_v29.*csv" || (IF csv_compressed THEN ".lz4" ELSE "")
       PREPROCESSED WITH (IF csv_compressed THEN "lz4 -d -c" ELSE "")
       THEN DELETE IF csv_delete
     <?=$dns?>;
 
-DEFINE http AS
+DEFINE LAZY http AS
   READ FROM
     FILES csv_prefix || "http_v29.*csv" || (IF csv_compressed THEN ".lz4" ELSE "")
       PREPROCESSED WITH (IF csv_compressed THEN "lz4 -d -c" ELSE "")
       THEN DELETE IF csv_delete
     <?=$http?>;
 
-DEFINE citrix AS
+DEFINE LAZY citrix AS
   READ FROM
     FILES csv_prefix || "citrix_v29.*csv" || (IF csv_compressed THEN ".lz4" ELSE "")
       PREPROCESSED WITH (IF csv_compressed THEN "lz4 -d -c" ELSE "")
       THEN DELETE IF csv_delete
     <?=$citrix?>;
 
-DEFINE citrix_chanless AS
+DEFINE LAZY citrix_chanless AS
   READ FROM
     FILES csv_prefix || "citrix_chanless_v29.*csv" || (IF csv_compressed THEN ".lz4" ELSE "")
       PREPROCESSED WITH (IF csv_compressed THEN "lz4 -d -c" ELSE "")
       THEN DELETE IF csv_delete
     <?=$citrix_chanless?>;
 
-DEFINE smb AS
+DEFINE LAZY smb AS
   READ FROM
     FILES csv_prefix || "smb_v29.*csv" || (IF csv_compressed THEN ".lz4" ELSE "")
       PREPROCESSED WITH (IF csv_compressed THEN "lz4 -d -c" ELSE "")
       THEN DELETE IF csv_delete
     <?=$smb?>;
 
-DEFINE sql AS
+DEFINE LAZY sql AS
   READ FROM
     FILES csv_prefix || "sql_v29.*csv" || (IF csv_compressed THEN ".lz4" ELSE "")
       PREPROCESSED WITH (IF csv_compressed THEN "lz4 -d -c" ELSE "")
       THEN DELETE IF csv_delete
     <?=$sql?>;
 
-DEFINE voip AS
+DEFINE LAZY voip AS
   READ FROM
     FILES csv_prefix || "voip_v29.*csv" || (IF csv_compressed THEN ".lz4" ELSE "")
       PREPROCESSED WITH (IF csv_compressed THEN "lz4 -d -c" ELSE "")
       THEN DELETE IF csv_delete
     <?=$voip?>;
 
-DEFINE tls AS
+DEFINE LAZY tls AS
   READ FROM
     FILES csv_prefix || "tls_v29.*csv" || (IF csv_compressed THEN ".lz4" ELSE "")
       PREPROCESSED WITH (IF csv_compressed THEN "lz4 -d -c" ELSE "")
--- ramen_root/sniffer/csv_kafka.php
+++ ramen_root/sniffer/csv_kafka.php
@@ -4,80 +4,80 @@
 PARAMETERS
   kafka_broker_list DEFAULTS TO "localhost:9092";
 
-DEFINE tcp AS
+DEFINE LAZY tcp AS
   READ FROM
     KAFKA TOPIC "pvx.tcp"
     WITH OPTIONS "metadata.broker.list" = kafka_broker_list
     <?=$tcp?>;
 
-DEFINE udp AS
+DEFINE LAZY udp AS
   READ FROM
     KAFKA TOPIC "pvx.udp"
     WITH OPTIONS "metadata.broker.list" = kafka_broker_list
     <?=$udp?>;
 
-DEFINE icmp AS
+DEFINE LAZY icmp AS
   READ FROM
     KAFKA TOPIC "pvx.icmp"
     WITH OPTIONS "metadata.broker.list" = kafka_broker_list
     <?=$icmp?>;
 
-DEFINE 'other-ip' AS
+DEFINE LAZY 'other-ip' AS
   READ FROM
     KAFKA TOPIC "pvx.other_ip"
     WITH OPTIONS "metadata.broker.list" = kafka_broker_list
     <?=$otherip?>;
 
 
-DEFINE 'non-ip' AS
+DEFINE LAZY 'non-ip' AS
   READ FROM
     KAFKA TOPIC "pvx.non_ip"
     WITH OPTIONS "metadata.broker.list" = kafka_broker_list
     <?=$nonip?>;
 
-DEFINE dns AS
+DEFINE LAZY dns AS
   READ FROM
     KAFKA TOPIC "pvx.dns"
     WITH OPTIONS "metadata.broker.list" = kafka_broker_list
     <?=$dns?>;
 
-DEFINE http AS
+DEFINE LAZY http AS
   READ FROM
     KAFKA TOPIC "pvx.http"
     WITH OPTIONS "metadata.broker.list" = kafka_broker_list
     <?=$http?>;
 
-DEFINE citrix AS
+DEFINE LAZY citrix AS
   READ FROM
     KAFKA TOPIC "pvx.citrix_channels"
     WITH OPTIONS "metadata.broker.list" = kafka_broker_list
     <?=$citrix?>;
 
-DEFINE citrix_chanless AS
+DEFINE LAZY citrix_chanless AS
   READ FROM
     KAFKA TOPIC "pvx.citrix"
     WITH OPTIONS "metadata.broker.list" = kafka_broker_list
     <?=$citrix_chanless?>;
 
-DEFINE smb AS
+DEFINE LAZY smb AS
   READ FROM
     KAFKA TOPIC "pvx.smb"
     WITH OPTIONS "metadata.broker.list" = kafka_broker_list
     <?=$smb?>;
 
-DEFINE sql AS
+DEFINE LAZY sql AS
   READ FROM
     KAFKA TOPIC "pvx.sql"
     WITH OPTIONS "metadata.broker.list" = kafka_broker_list
     <?=$sql?>;
 
-DEFINE voip AS
+DEFINE LAZY voip AS
   READ FROM
     KAFKA TOPIC "pvx.voip"
     WITH OPTIONS "metadata.broker.list" = kafka_broker_list
     <?=$voip?>;
 
-DEFINE tls AS
+DEFINE LAZY tls AS
   READ FROM
     KAFKA TOPIC "pvx.tls"
     WITH OPTIONS "metadata.broker.list" = kafka_broker_list
