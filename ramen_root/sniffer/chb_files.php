088b8dc1315b09573ce9ba01b696c6834ee644e0
Cedric Cellier <rixed@happyleptic.org>
2019-11-21 16:32:23 +0100

    Fix: junkie output file names have changed (again)
    (cherry picked from commit 962cbf9984337c5d84f910df730ff6049840eecd)

--- ramen_root/sniffer/chb_files.php
+++ ramen_root/sniffer/chb_files.php
@@ -8,91 +8,91 @@ PARAMETERS
 
 DEFINE LAZY tcp_ext AS
   READ FROM
-    FILES files_prefix || "tcp_v30.*chb" || (IF files_compressed THEN ".lz4" ELSE "")
+    FILES files_prefix || "tcp_*v30.*chb" || (IF files_compressed THEN ".lz4" ELSE "")
       PREPROCESSED WITH (IF files_compressed THEN "lz4 -d -c" ELSE "")
       THEN DELETE IF files_delete
     <?=$tcp?>;
 
 DEFINE LAZY udp_ext AS
   READ FROM
-    FILES files_prefix || "udp_v30.*chb" || (IF files_compressed THEN ".lz4" ELSE "")
+    FILES files_prefix || "udp_*v30.*chb" || (IF files_compressed THEN ".lz4" ELSE "")
       PREPROCESSED WITH (IF files_compressed THEN "lz4 -d -c" ELSE "")
       THEN DELETE IF files_delete
     <?=$udp?>;
 
 DEFINE LAZY icmp_ext AS
   READ FROM
-    FILES files_prefix || "icmp_v30.*chb" || (IF files_compressed THEN ".lz4" ELSE "")
+    FILES files_prefix || "icmp_*v30.*chb" || (IF files_compressed THEN ".lz4" ELSE "")
       PREPROCESSED WITH (IF files_compressed THEN "lz4 -d -c" ELSE "")
       THEN DELETE IF files_delete
     <?=$icmp?>;
 
 DEFINE LAZY 'other-ip_ext' AS
   READ FROM
-    FILES files_prefix || "other_ip_v30.*chb" || (IF files_compressed THEN ".lz4" ELSE "")
+    FILES files_prefix || "other_ip_*v30.*chb" || (IF files_compressed THEN ".lz4" ELSE "")
       PREPROCESSED WITH (IF files_compressed THEN "lz4 -d -c" ELSE "")
       THEN DELETE IF files_delete
     <?=$otherip?>;
 
 DEFINE LAZY 'non-ip_ext' AS
   READ FROM
-    FILES files_prefix || "non_ip_v30.*chb" || (IF files_compressed THEN ".lz4" ELSE "")
+    FILES files_prefix || "non_ip_*v30.*chb" || (IF files_compressed THEN ".lz4" ELSE "")
       PREPROCESSED WITH (IF files_compressed THEN "lz4 -d -c" ELSE "")
       THEN DELETE IF files_delete
     <?=$nonip?>;
 
 DEFINE LAZY dns_ext AS
   READ FROM
-    FILES files_prefix || "dns_v30.*chb" || (IF files_compressed THEN ".lz4" ELSE "")
+    FILES files_prefix || "dns_*v30.*chb" || (IF files_compressed THEN ".lz4" ELSE "")
       PREPROCESSED WITH (IF files_compressed THEN "lz4 -d -c" ELSE "")
       THEN DELETE IF files_delete
     <?=$dns?>;
 
 DEFINE LAZY http_ext AS
   READ FROM
-    FILES files_prefix || "http_v30.*chb" || (IF files_compressed THEN ".lz4" ELSE "")
+    FILES files_prefix || "http_*v30.*chb" || (IF files_compressed THEN ".lz4" ELSE "")
       PREPROCESSED WITH (IF files_compressed THEN "lz4 -d -c" ELSE "")
       THEN DELETE IF files_delete
     <?=$http?>;
 
 DEFINE LAZY citrix_channels_ext AS
   READ FROM
-    FILES files_prefix || "citrix_channels_v30.*chb" || (IF files_compressed THEN ".lz4" ELSE "")
+    FILES files_prefix || "citrix_channels_*v30.*chb" || (IF files_compressed THEN ".lz4" ELSE "")
       PREPROCESSED WITH (IF files_compressed THEN "lz4 -d -c" ELSE "")
       THEN DELETE IF files_delete
     <?=$citrix_channels?>;
 
 DEFINE LAZY citrix_ext AS
   READ FROM
-    FILES files_prefix || "citrix_v30.*chb" || (IF files_compressed THEN ".lz4" ELSE "")
+    FILES files_prefix || "citrix_*v30.*chb" || (IF files_compressed THEN ".lz4" ELSE "")
       PREPROCESSED WITH (IF files_compressed THEN "lz4 -d -c" ELSE "")
       THEN DELETE IF files_delete
     <?=$citrix?>;
 
 DEFINE LAZY smb_ext AS
   READ FROM
-    FILES files_prefix || "smb_v30.*chb" || (IF files_compressed THEN ".lz4" ELSE "")
+    FILES files_prefix || "smb_*v30.*chb" || (IF files_compressed THEN ".lz4" ELSE "")
       PREPROCESSED WITH (IF files_compressed THEN "lz4 -d -c" ELSE "")
       THEN DELETE IF files_delete
     <?=$smb?>;
 
 DEFINE LAZY sql_ext AS
   READ FROM
-    FILES files_prefix || "databases_v30.*chb" || (IF files_compressed THEN ".lz4" ELSE "")
+    FILES files_prefix || "databases_*v30.*chb" || (IF files_compressed THEN ".lz4" ELSE "")
       PREPROCESSED WITH (IF files_compressed THEN "lz4 -d -c" ELSE "")
       THEN DELETE IF files_delete
     <?=$sql?>;
 
 DEFINE LAZY voip_ext AS
   READ FROM
-    FILES files_prefix || "voip_v30.*chb" || (IF files_compressed THEN ".lz4" ELSE "")
+    FILES files_prefix || "voip_*v30.*chb" || (IF files_compressed THEN ".lz4" ELSE "")
       PREPROCESSED WITH (IF files_compressed THEN "lz4 -d -c" ELSE "")
       THEN DELETE IF files_delete
     <?=$voip?>;
 
 DEFINE LAZY tls_ext AS
   READ FROM
-    FILES files_prefix || "tls_v30.*chb" || (IF files_compressed THEN ".lz4" ELSE "")
+    FILES files_prefix || "tls_*v30.*chb" || (IF files_compressed THEN ".lz4" ELSE "")
       PREPROCESSED WITH (IF files_compressed THEN "lz4 -d -c" ELSE "")
       THEN DELETE IF files_delete
     <?=$tls?>;
--- ramen_root/sniffer/csv_files.php
+++ ramen_root/sniffer/csv_files.php
@@ -8,91 +8,91 @@ PARAMETERS
 
 DEFINE LAZY tcp AS
   READ FROM
-    FILES files_prefix || "tcp_v30.*csv" || (IF files_compressed THEN ".lz4" ELSE "")
+    FILES files_prefix || "tcp_*v30.*csv" || (IF files_compressed THEN ".lz4" ELSE "")
       PREPROCESSED WITH (IF files_compressed THEN "lz4 -d -c" ELSE "")
       THEN DELETE IF files_delete
     <?=$tcp?>;
 
 DEFINE LAZY udp AS
   READ FROM
-    FILES files_prefix || "udp_v30.*csv" || (IF files_compressed THEN ".lz4" ELSE "")
+    FILES files_prefix || "udp_*v30.*csv" || (IF files_compressed THEN ".lz4" ELSE "")
       PREPROCESSED WITH (IF files_compressed THEN "lz4 -d -c" ELSE "")
       THEN DELETE IF files_delete
     <?=$udp?>;
 
 DEFINE LAZY icmp AS
   READ FROM
-    FILES files_prefix || "icmp_v30.*csv" || (IF files_compressed THEN ".lz4" ELSE "")
+    FILES files_prefix || "icmp_*v30.*csv" || (IF files_compressed THEN ".lz4" ELSE "")
       PREPROCESSED WITH (IF files_compressed THEN "lz4 -d -c" ELSE "")
       THEN DELETE IF files_delete
     <?=$icmp?>;
 
 DEFINE LAZY 'other-ip' AS
   READ FROM
-    FILES files_prefix || "other_ip_v30.*csv" || (IF files_compressed THEN ".lz4" ELSE "")
+    FILES files_prefix || "other_ip_*v30.*csv" || (IF files_compressed THEN ".lz4" ELSE "")
       PREPROCESSED WITH (IF files_compressed THEN "lz4 -d -c" ELSE "")
       THEN DELETE IF files_delete
     <?=$otherip?>;
 
 DEFINE LAZY 'non-ip' AS
   READ FROM
-    FILES files_prefix || "non_ip_v30.*csv" || (IF files_compressed THEN ".lz4" ELSE "")
+    FILES files_prefix || "non_ip_*v30.*csv" || (IF files_compressed THEN ".lz4" ELSE "")
       PREPROCESSED WITH (IF files_compressed THEN "lz4 -d -c" ELSE "")
       THEN DELETE IF files_delete
     <?=$nonip?>;
 
 DEFINE LAZY dns AS
   READ FROM
-    FILES files_prefix || "dns_v30.*csv" || (IF files_compressed THEN ".lz4" ELSE "")
+    FILES files_prefix || "dns_*v30.*csv" || (IF files_compressed THEN ".lz4" ELSE "")
       PREPROCESSED WITH (IF files_compressed THEN "lz4 -d -c" ELSE "")
       THEN DELETE IF files_delete
     <?=$dns?>;
 
 DEFINE LAZY http AS
   READ FROM
-    FILES files_prefix || "http_v30.*csv" || (IF files_compressed THEN ".lz4" ELSE "")
+    FILES files_prefix || "http_*v30.*csv" || (IF files_compressed THEN ".lz4" ELSE "")
       PREPROCESSED WITH (IF files_compressed THEN "lz4 -d -c" ELSE "")
       THEN DELETE IF files_delete
     <?=$http?>;
 
 DEFINE LAZY citrix_channels AS
   READ FROM
-    FILES files_prefix || "citrix_channels_v30.*csv" || (IF files_compressed THEN ".lz4" ELSE "")
+    FILES files_prefix || "citrix_channels_*v30.*csv" || (IF files_compressed THEN ".lz4" ELSE "")
       PREPROCESSED WITH (IF files_compressed THEN "lz4 -d -c" ELSE "")
       THEN DELETE IF files_delete
     <?=$citrix_channels?>;
 
 DEFINE LAZY citrix AS
   READ FROM
-    FILES files_prefix || "citrix_v30.*csv" || (IF files_compressed THEN ".lz4" ELSE "")
+    FILES files_prefix || "citrix_*v30.*csv" || (IF files_compressed THEN ".lz4" ELSE "")
       PREPROCESSED WITH (IF files_compressed THEN "lz4 -d -c" ELSE "")
       THEN DELETE IF files_delete
     <?=$citrix?>;
 
 DEFINE LAZY smb AS
   READ FROM
-    FILES files_prefix || "smb_v30.*csv" || (IF files_compressed THEN ".lz4" ELSE "")
+    FILES files_prefix || "smb_*v30.*csv" || (IF files_compressed THEN ".lz4" ELSE "")
       PREPROCESSED WITH (IF files_compressed THEN "lz4 -d -c" ELSE "")
       THEN DELETE IF files_delete
     <?=$smb?>;
 
 DEFINE LAZY sql AS
   READ FROM
-    FILES files_prefix || "sql_v30.*csv" || (IF files_compressed THEN ".lz4" ELSE "")
+    FILES files_prefix || "sql_*v30.*csv" || (IF files_compressed THEN ".lz4" ELSE "")
       PREPROCESSED WITH (IF files_compressed THEN "lz4 -d -c" ELSE "")
       THEN DELETE IF files_delete
     <?=$sql?>;
 
 DEFINE LAZY voip AS
   READ FROM
-    FILES files_prefix || "voip_v30.*csv" || (IF files_compressed THEN ".lz4" ELSE "")
+    FILES files_prefix || "voip_*v30.*csv" || (IF files_compressed THEN ".lz4" ELSE "")
       PREPROCESSED WITH (IF files_compressed THEN "lz4 -d -c" ELSE "")
       THEN DELETE IF files_delete
     <?=$voip?>;
 
 DEFINE LAZY tls AS
   READ FROM
-    FILES files_prefix || "tls_v30.*csv" || (IF files_compressed THEN ".lz4" ELSE "")
+    FILES files_prefix || "tls_*v30.*csv" || (IF files_compressed THEN ".lz4" ELSE "")
       PREPROCESSED WITH (IF files_compressed THEN "lz4 -d -c" ELSE "")
       THEN DELETE IF files_delete
     <?=$tls?>;
