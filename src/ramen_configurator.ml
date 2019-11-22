7df6e043eb91709b22152200e9225e4a6f031ede
Damien Bihel <dbihel@accedian.com>
2019-11-22 14:45:44 +0100

    log: add info with version when started.
    (cherry picked from commit e9aa4e46369c695813957bdc96e94dd07e542772)

--- src/ramen_configurator.ml
+++ src/ramen_configurator.ml
@@ -326,7 +326,7 @@ let start debug monitor ramen_cmd root_dir confserver_url db_name
           notif_conf_file identity_file_ to_sqlite
           kafka_options kafka_topic kafka_partition
           tenant_id tenant_name
-          dry_run_ =
+          dry_run_ version =
   logger := make_logger debug ;
   !logger.info "Run ramen-configurator version %s" version ;
   identity_file := identity_file_ ;
@@ -502,7 +502,8 @@ let start_cmd =
       $ kafka_partition
       $ tenant_id
       $ tenant_name
-      $ dry_run),
+      $ dry_run
+      $ const version),
     info "ramen_configurator" ~version ~doc)
 
 let () =
