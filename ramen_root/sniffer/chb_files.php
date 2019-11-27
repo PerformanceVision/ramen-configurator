51a251f58f3454fc6f52d705b29ec563c2025038
Cedric Cellier <rixed@happyleptic.org>
2019-11-27 13:43:09 +0100

    Cosmetics: simpler data structure, names

--- ramen_root/sniffer/chb_files.php
+++ ramen_root/sniffer/chb_files.php
@@ -7,22 +7,21 @@ PARAMETERS
   files_delete DEFAULTS TO true;
 
 <?
-foreach ($PROTOCOLS as $protocol) {
-$content_path = get_content_path($protocol);
-$content = file_get_contents("$content_path");
-$ramen_function_name = get_ramen_function_name($protocol);
-echo <<<EOT
-DEFINE LAZY '{$ramen_function_name}_ext' AS
+foreach ($LAYERS as $layer => $ramen_function_name) {
+  $content_path = get_content_path($layer);
+  $content = file_get_contents($content_path);
+?>
+DEFINE LAZY '<?=$ramen_function_name?>_ext' AS
   READ FROM
-    FILES files_prefix || "{$protocol["sniffer"]}_*v30.*chb" || (IF files_compressed THEN ".lz4" ELSE "")
+    FILES files_prefix || "<?=$layer?>_*v30.*chb" || (IF files_compressed THEN ".lz4" ELSE "")
       PREPROCESSED WITH (IF files_compressed THEN "lz4 -d -c" ELSE "")
       THEN DELETE IF files_delete
     AS ROWBINARY (
-      $content
+      <?=$content?>
   )
   EVENT STARTING AT capture_begin * 1e-6
-    AND STOPPING AT capture_end * 1e-6;\n
-EOT;
+    AND STOPPING AT capture_end * 1e-6;
+<?
 }
 ?>
 
--- ramen_root/sniffer/chb_v30.php
+++ ramen_root/sniffer/chb_v30.php
@@ -1,26 +1,21 @@
 <?
-$PROTOCOLS = [
-  ["sniffer" =>"tcp"],
-  ["sniffer" =>"udp"],
-  ["sniffer" =>"icmp"],
-  ["sniffer" =>"other_ip", "ramen_function_name" => "other-ip"],
-  ["sniffer" =>"non_ip", "ramen_function_name" => "non-ip"],
-  ["sniffer" =>"dns"],
-  ["sniffer" =>"http"],
-  ["sniffer" =>"citrix_channels"],
-  ["sniffer" =>"citrix"],
-  ["sniffer" =>"smb"],
-  ["sniffer" =>"databases", "ramen_function_name" => "sql"],
-  ["sniffer" =>"voip"],
-  ["sniffer" =>"tls"],
+$LAYERS = [
+  "tcp" => "tcp",
+  "udp" => "udp",
+  "icmp" =>"icmp",
+  "other_ip" => "other-ip",
+  "non_ip" => "non-ip",
+  "dns" => "dns",
+  "http" => "http",
+  "citrix_channels" => "citrix_channels",
+  "citrix" => "citrix",
+  "smb" => "smb",
+  "databases" => "sql",
+  "voip" => "voip",
+  "tls" => "tls",
 ];
 
-function get_ramen_function_name($protocol) {
-  return $protocol["ramen_function_name"] ?: $protocol["sniffer"];
+function get_content_path($layer) {
+  return "ramen_root/sniffer/{$layer}.txt";
 }
-
-function get_content_path($protocol) {
-  return "ramen_root/sniffer/{$protocol["sniffer"]}.txt";
-}
-
 ?>
