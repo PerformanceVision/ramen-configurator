135b9faa8bdfa5867aebdfbbfa7ced3ed5ebab3b
Cedric Cellier <rixed@happyleptic.org>
2019-11-27 13:43:09 +0100

    Cosmetics: simpler data structure, names

--- ramen_root/sniffer/chb_kafka.php
+++ ramen_root/sniffer/chb_kafka.php
@@ -6,23 +6,22 @@ PARAMETERS
   kafka_max_msg_size DEFAULTS TO "1000000";
 
 <?
-foreach ($PROTOCOLS as $protocol) {
-$content_path = get_content_path($protocol);
-$content = file_get_contents("$content_path");
-$ramen_function_name = get_ramen_function_name($protocol);
-echo <<<EOT
-DEFINE LAZY '{$ramen_function_name}_ext' AS
+foreach ($LAYERS as $layer => $ramen_function_name) {
+  $content_path = get_content_path($layer);
+  $content = file_get_contents($content_path);
+?>
+DEFINE LAZY '<?=$ramen_function_name?>_ext' AS
   READ FROM
-    KAFKA TOPIC "pvx.chb.{$protocol["sniffer"]}"
+    KAFKA TOPIC "pvx.chb.<?=$layer?>"
     WITH OPTIONS
       "metadata.broker.list" = kafka_broker_list,
       "max.partition.fetch.bytes" = kafka_max_msg_size
     AS ROWBINARY (
-      $content
+      <?=$content?>
   )
   EVENT STARTING AT capture_begin * 1e-6
-    AND STOPPING AT capture_end * 1e-6;\n
-EOT;
+    AND STOPPING AT capture_end * 1e-6;
+<?
 }
 ?>
 
